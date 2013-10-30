0db290952b1061b2b58d113d7c04a69efcc39434
Mesut Tasci <slackermesut@gmail.com>
2013-10-30 10:16:25 +0200

    y alias added

--- .bin/aliases.zsh
+++ .bin/aliases.zsh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
-gito() {
-    # git outside working dir
-    # gito <repo-dir> status 
-    git --git-dir=$PWD/$1/.git --work-tree=$PWD/$1 $2
-}
+
+# Arch linux specific aliases
+alias y=yaourt
