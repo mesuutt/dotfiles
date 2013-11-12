2891a3d2ef1ad693a11b01c35862124ba803f096
Mesut Tasci <slackermesut@gmail.com>
2013-11-12 14:38:29 +0200

    aliases updated

--- .bin/aliases.zsh
+++ .bin/aliases.zsh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
 
+alias h='howdoi'
+
+alias http='noglob http'
+alias git='noglob git'
+alias find='noglob find'
+
+
 # Arch linux specific aliases
-alias y=yaourt
+alias y='yaourt'
