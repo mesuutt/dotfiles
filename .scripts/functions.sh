10686153f588d924b30b9ca2b57dbc7ddbb0c27b
Mesut Tasci <slackermesut@gmail.com>
2014-01-29 02:02:46 +0200

    Move dotfiles to specific dirs

--- .scripts/functions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-gito() {
-    # git outside working dir
-    # gito <repo-dir> status 
-    git --git-dir=$PWD/$1/.git --work-tree=$PWD/$1 $2
-}
---
+++ dots/aliases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+alias h='howdoi'
+
+alias http='noglob http'
+alias git='noglob git'
+alias find='noglob find'
+alias pip='noglob pip'
+
+alias gap='git add -p'
+
+# Arch linux specific aliases
+alias y='yaourt'
+alias pkg-update='sudo etckeeper pre-install && sudo pacman -Syu && sudo etckeeper post-install'
+
+
+alias vup="vagrant up"
+alias vsh="vagrant ssh"
+alias vsp="vagrant suspend"
