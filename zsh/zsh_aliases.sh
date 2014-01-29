2019c94a5e5796454207ec35d41346d42466dfaa
Mesut Tasci <slackermesut@gmail.com>
2014-01-29 11:26:19 +0200

    zsh & bash specific aliases updated

---
+++ zsh/zsh_aliases.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# Aliases with + zsh specific aliases
+
+test ~/.aliases.sh && source ~/.aliases.sh
+
+
+alias http='noglob http'
+alias git='noglob git'
+alias find='noglob find'
+alias pip='noglob pip'
+
+
